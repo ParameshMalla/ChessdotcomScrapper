5d4b164bce3313322dafde2ad4a5a39e475f951b
ParameshMalla <parameshmalla1996@gmail.com>
2020-12-19 20:24:55 +0530

    Add download folder for whole games

--- scrap.py
+++ scrap.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
 import time
+import os
 
-# # function to take care of downloading file
-# def enable_download_headless(browser,download_dir):
-#     browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-#     params = {'cmd':'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
-#     browser.execute("send_command", params)
+# function to take care of downloading file
+def enable_download_headless(browser,download_dir):
+    browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+    params = {'cmd':'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+    browser.execute("send_command", params)
 
 options = Options()
 options.binary_location = r"C:\Program Files (x86)\BraveSoftware\Brave-Browser\Application\brave.exe"
@@ -26,11 +27,27 @@ opening = Select(driver.find_element_by_name('opening'))
 openings = []
 for Openingoptions in opening.options:
     openings.append(Openingoptions.text)
-driver.close
 
 # Strips all the whitespaces and /n character
 openings.remove(openings[0])
 for i in range(len(openings)):
     openings[i] = openings[i].strip()
+    openings[i] = openings[i].replace(' ','%20')
+    openings[i] = openings[i].replace('-','_')
+    url = "https://www.chess.com/games/archive/hikaru?gameOwner=other_game&gameType=live&opening="+openings[i]+"&timeSort=desc&gameTypes%5B0%5D=chess960&gameTypes%5B1%5D=daily"
+    driver.get(url)
+    driver.find_element_by_xpath('//*[@id="games-root-index"]/div/div[1]/div/nav/button[7]').click()
+    pageUrl = driver.current_url
+    noOfPages = int(pageUrl[-2:])
+    if not os.path.exists(openings[i]):
+            os.makedirs(openings[i])
+    pgns = os.path.join(r'D:\Programming\WebScraping\ChessdotcomScrapper', openings[i])
+    enable_download_headless(driver, pgns)
+    for j in range(noOfPages,1,-1):
+        pageUrl[-2:] = str(j)
+        driver.get(pageUrl)
+        driver.find_element_by_xpath('//*[@id="games-root-index"]/div/div[2]/div/table/thead/tr/th[7]/input').click()
+        driver.find_element_by_xpath('//*[@id="games-root-index"]/div/header/div/button/div/span').click()
 
+driver.close
 
